57ecc665f2a1ac57efeb83a8ed3496a3770d5a8b
modul10 <alvithops@gmail.com>
2026-02-05 15:19:00 +0700

    first commit

---
+++ latihan1.php
@@ -0,0 +1,16 @@
+<?php
+5
+// memulai sesi
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+// Set variabel sesi
+$_SESSION["username"] = "ika";
+$_SESSION["nama"] = "Kartika Fatmawati";
+echo "Variabel sesi telah diciptakan.";
+?>
+</body>
+</html>
