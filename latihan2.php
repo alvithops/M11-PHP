26d6e061080f28f35bdaecefc41e4a48ced58d7f
modul10 <alvithops@gmail.com>
2026-02-05 15:25:24 +0700

    Update konfigurasi profile dan tambah konten 4

--- latihan2.php
+++ latihan2.php
@@ -3,11 +3,13 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+
 <body>
-<?php
-// Echo variabel sesi yang telah diset di halaman sebelumnya
-echo "Username: " . $_SESSION["username"]. ".<br>";
-echo "Nama: " . $_SESSION["nama"]. ".";
-?>
+    <?php
+    // Echo variabel sesi yang telah diset di halaman sebelumnya
+    echo "Username: " . $_SESSION["username"] . ".<br>";
+    echo "Nama: " . $_SESSION["nama"] . ".";
+    ?>
 </body>
+
 </html>
