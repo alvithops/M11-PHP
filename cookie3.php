9a5dbfae083f851eb5ccb504396534c6e26dae60
modul10 <alvithops@gmail.com>
2026-02-05 20:45:19 +0700

    Update konfigurasi profile dan tambah konten 29

--- cookie3.php
+++ cookie3.php
@@ -5,9 +5,11 @@ setcookie("namauser", "", time() - 3600);
 <!DOCTYPE html>
 <html>
 15
+
 <body>
-<?php
-echo "Cookie 'namauser' telah dihapus.";
-?>
+    <?php
+    echo "Cookie 'namauser' telah dihapus.";
+    ?>
 </body>
+
 </html>
