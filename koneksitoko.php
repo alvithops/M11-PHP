976e1f0043362c8d214e18a16e714cf670893c21
modul10 <alvithops@gmail.com>
2026-02-05 15:56:07 +0700

    Update konfigurasi profile dan tambah konten 9

--- koneksitoko.php
+++ koneksitoko.php
@@ -1,15 +1,20 @@
 <?php
-function koneksiToko(){
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "toko";
-// menciptakan koneksi
-$koneksi = mysqli_connect($servername, $username, $password, $dbname);
-// Cek koneksi
-if (!$koneksi) {
-die("Koneksi gagal: " . mysqli_error());
+function koneksitoko()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "toko";
+
+    // menciptakan koneksi
+    $koneksi = mysqli_connect($servername, $username, $password, $dbname);
+
+    // cek koneksi
+    if (!$koneksi) {
+        // perbaikan: mysqli_error membutuhkan parameter koneksi
+        die("koneksi gagal: " . mysqli_connect_error());
+    }
+
+    return $koneksi;
 }
-return $koneksi;
-4
-}
+?>
