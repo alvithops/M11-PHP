e25c96e7df9ca46d8325ecb9c7e507339e4f498a
modul10 <alvithops@gmail.com>
2026-02-05 20:44:43 +0700

    Update konfigurasi profile dan tambah konten 28

---
+++ cookie3.php
@@ -0,0 +1,13 @@
+<?php
+// set expiration date ke satu jam yang lalu
+setcookie("namauser", "", time() - 3600);
+?>
+<!DOCTYPE html>
+<html>
+15
+<body>
+<?php
+echo "Cookie 'namauser' telah dihapus.";
+?>
+</body>
+</html>
