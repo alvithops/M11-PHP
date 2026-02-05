54e76f7ebd7624b8fb67fdd61e3c0db6453e8394
modul10 <alvithops@gmail.com>
2026-02-05 15:26:44 +0700

    Update konfigurasi profile dan tambah konten 5

--- latihan3.php
+++ latihan3.php
@@ -3,12 +3,14 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+
 <body>
-<?php
-// Mengubah variabel sesi
-$_SESSION["nama"] = "Kartika Sari";
-echo "Variabel sesi telah diubah.";
-?>
+    <?php
+    // Mengubah variabel sesi
+    $_SESSION["nama"] = "Kartika Sari";
+    echo "Variabel sesi telah diubah.";
+    ?>
 </body>
 6
+
 </html>
