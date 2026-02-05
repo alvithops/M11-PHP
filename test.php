38ae39e03b45381f5abffba44b2a31e4cc7fa78c
modul10 <alvithops@gmail.com>
2026-02-05 16:01:01 +0700

    Update konfigurasi profile dan tambah konten 13

--- test.php
+++ test.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
+
 <body>
-<?php
-require_once ('user.php');
-$username="susi";
-$password="abc";
-if(otentik($username, $password))
-echo "Berhasil";
-else
-echo "Gagal";
-?>
+    <?php
+    require_once('user.php');
+    $username = "susi";
+    $password = "abc";
+    if (otentik($username, $password))
+        echo "Berhasil";
+    else
+        echo "Gagal";
+    ?>
 </body>
+
 </html>
