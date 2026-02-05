2fc146a36a0ec4226d6eca5f733d605b46ac414c
modul10 <alvithops@gmail.com>
2026-02-05 15:27:44 +0700

    Update konfigurasi profile dan tambah konten 6

--- latihan4.php
+++ latihan4.php
@@ -3,13 +3,15 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+
 <body>
-<?php
-// menghapus semua variabel sesi
-session_unset();
-// menghancurkan sesi
-session_destroy();
-?>
-Sesi telah dihapus
+    <?php
+    // menghapus semua variabel sesi
+    session_unset();
+    // menghancurkan sesi
+    session_destroy();
+    ?>
+    Sesi telah dihapus
 </body>
+
 </html>
