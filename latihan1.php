c7fc674bf9f2dfa8037601ac72f8846fa4041959
modul10 <alvithops@gmail.com>
2026-02-05 15:23:53 +0700

    Update konfigurasi profile dan tambah konten 2

--- latihan1.php
+++ latihan1.php
@@ -4,17 +4,20 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <title>latihan session</title>
 </head>
-<body>
-<?php
-// set variabel sesi dengan huruf kecil sesuai preferensi
-$_SESSION["username"] = "ika";
-$_SESSION["nama"] = "kartika fatmawati";
 
-echo "variabel sesi telah diciptakan.";
-?>
+<body>
+    <?php
+    // set variabel sesi dengan huruf kecil sesuai preferensi
+    $_SESSION["username"] = "ika";
+    $_SESSION["nama"] = "kartika fatmawati";
+
+    echo "variabel sesi telah diciptakan.";
+    ?>
 </body>
+
 </html>
